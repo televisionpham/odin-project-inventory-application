d5c9bd79af5ee56bee9eb732dd5e92f14f43395f
Van Pham <televisionpham@gmail.com>
2023-01-30 12:20:50 +0700

    completed category

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
 
 exports.categoryList = (req, res, next) => {
   Category.find({})
@@ -9,4 +10,75 @@ exports.categoryList = (req, res, next) => {
       }
       res.render("categoryList", { title: "Category List", categoryList });
     });
+};
+
+exports.categoryDetail = (req, res, next) => {
+  Category.findById(req.params.id).exec(function (err, category) {
+    if (err) {
+      return next(err);
+    }
+
+    Item.find({ category: category }).exec(function (err, itemList) {
+      if (err) {
+        return next(err);
+      }
+
+      res.render("categoryDetail", { category, itemList });
+    });
+  });
+};
+
+exports.create_get = (req, res, next) => {
+  res.render("categoryCreate");
+};
+
+exports.create_post = (req, res, next) => {
+  const categoryDetail = {
+    name: req.body.name,
+    description: req.body.description,
+  };
+
+  const category = new Category(categoryDetail);
+  category.save(function (err) {
+    if (err) {
+      return next(err);
+    }
+
+    res.redirect("/category");
+  });
+};
+
+exports.update_get = (req, res, next) => {
+  Category.findById(req.params.id).exec(function (err, category) {
+    if (err) {
+      return next(err);
+    }
+    res.render("categoryUpdate", { category });
+  });
+};
+
+exports.update_post = (req, res, next) => {
+  const categoryDetail = {
+    name: req.body.name,
+    description: req.body.description,
+  };
+  Category.findByIdAndUpdate(req.params.id, categoryDetail).exec(function (
+    err
+  ) {
+    if (err) {
+      return next(err);
+    }
+
+    res.redirect("/category");
+  });
+};
+
+exports.delete = (req, res, next) => {
+  Category.findByIdAndRemove(req.params.id).exec(function (err) {
+    if (err) {
+      return next(err);
+    }
+
+    res.redirect("/category");
+  });
 };
--- routes/categories.js
+++ routes/categories.js
@@ -4,5 +4,14 @@ var router = express.Router();
 
 /* GET categories listing. */
 router.get("/", categoryControler.categoryList);
+router.get("/:id", categoryControler.categoryDetail);
+
+router.get("/create", categoryControler.create_get);
+router.post("/create", categoryControler.create_post);
+
+router.get("/:id/update", categoryControler.update_get);
+router.post("/:id/update", categoryControler.update_post);
+
+router.post("/:id/delete", categoryControler.delete);
 
 module.exports = router;
